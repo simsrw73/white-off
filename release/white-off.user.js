e26afee924cc5aa1a01a82360409dc9797aa66bb
Randy W. Sims <simsrw73@gmail.com>
2018-08-31 20:30:42 -0400

    Update @updateURL meta

--- release/white-off.user.js
+++ release/white-off.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name          White-Off for Faithlife Community Forums
 // @description   A low contrast, modern theme for the Faithlife Community Forums
-// @version       1.0.5
+// @version       1.0.6
 // @author        Randy W. Sims <simsrw73@gmail.com>
 // @license       MIT
 // @namespace     https://github.com/simsrw73
 // @homepageURL   https://github.com/simsrw73/white-off
 // @supportURL    https://github.com/simsrw73/white-off/issues
-// @updateURL     https://github.com/simsrw73/white-off/raw/master/release/white-off.meta.js
-// @downloadURL   https://github.com/simsrw73/white-off/raw/master/release/white-off.user.js
+// @updateURL     https://raw.githubusercontent.com/simsrw73/white-off/master/release/white-off.meta.js
+// @downloadURL   https://raw.githubusercontent.com/simsrw73/white-off/master/release/white-off.user.js
 // @run-at        document-start
 // @include       http://community.logos.com/*
 // @include       https://community.logos.com/*
